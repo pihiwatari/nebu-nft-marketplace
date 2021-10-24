17c0b03030526d36b6d8c467faf7afe858ee07ab
erayas <eliasrayasg@gmail.com>
2021-10-23 19:05:58 -0500

    Finished first desktop draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        //grid auto fill columns
+        auto: "repeat(auto-fill, minmax(400px, 1fr))",
+      },
+      height: {
+        "screen-50": "50vh",
+        "screen-80": "80vh",
+      },
+    },
   },
   variants: {
     extend: {},
