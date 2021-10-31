5e9ffadb36c0042d5cd06b46f2a7506b2a3b50d2
erayas <eliasrayasg@gmail.com>
2021-10-30 20:16:17 -0500

    added menu fucntionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles/styles.css";
+
+// open close mobile menu
+
+// closed menu icon
+const $hamMenu = document.querySelector("#menu-icon");
+
+//mobile nav menu
+const $mobileMenu = document.querySelector("#mobile-menu");
+
+$hamMenu.addEventListener("click", () => {
+  $mobileMenu.classList.toggle("hidden");
+});
+$mobileMenu.addEventListener("click", () => {
+  $mobileMenu.classList.toggle("hidden");
+});
